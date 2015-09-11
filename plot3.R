c13befdd7a91d4085a25c832dbe817b12e8591f5
Aaron Graham <aarcg@yahoo.com>
2015-09-11 08:12:40 -0400

    Cleaned up some code formatting

--- plot3.R
+++ plot3.R
@@ -28,7 +28,7 @@ legend("topright",
        legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
        lty = c(1, 1, 1),
        col = c("black", "red", "blue")
-       )
+)
 
 ## End the png recording
 dev.off()
